27459a2dc1aa025217f3c18bb79968efafbe6136
Panagiotis Gouvas <pgouvas@ubitech.eu>
2012-07-24 18:57:41 +0300

    Added Testing Framework

---
+++ src/test/java/test/TestParser.java
@@ -0,0 +1,33 @@
+package test;
+
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: pgouvas
+ * Date: 7/24/12
+ * Time: 5:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+public class TestParser {
+
+    @Test
+    public void testMethod1(){
+        System.out.println("Hello1");
+        int i=0;
+        int x = 1/i;
+
+    }
+
+
+    @Test
+    public void testMethod2(){
+        System.out.println("Hello2");
+
+
+    }
+
+
+
+}
