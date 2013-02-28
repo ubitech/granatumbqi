d52ba0757195efabd0987263076cd85d5b7f7efe
Panagiotis Gouvas <pgouvas@ubitech.eu>
2013-02-28 11:53:11 +0200

    sparql namespace checking

--- src/main/java/eu/granatum/beans/DataCloudSPARQLInterface.java
+++ src/main/java/eu/granatum/beans/DataCloudSPARQLInterface.java
@@ -76,9 +76,9 @@ public class DataCloudSPARQLInterface
         {
 //            System.out.println(line);
             
-            if(line.startsWith("<sparql"))
-                msgsock.append("<sparql>\n");
-            else
+//            if(line.startsWith("<sparql"))
+//                msgsock.append("<sparql>\n");
+//            else
                 msgsock.append(line+"\n");
             
 //            msgsock.append(line+"\n");            
